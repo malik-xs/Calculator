5f7b4eb18ac5e18f3c227fb5e49a6d84366cff1f
mohon-xs <mohon@xpeedstudio.com>
2021-03-23 14:34:15 +0600

    Update calculator.js

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -4,14 +4,21 @@
 function Calc() {
 }
 
+Calc.prototype.set = function (a, b) {
+	this._a = a;
+	this._b = b;
+};
+
 Calc.prototype.add = function (a, b) {
-	return a + b;
+	return this._a + this._b;
 };
 
 Calc.prototype.sub = function (a, b) {
-	return a - b;
+	return this._a - this._b;
 };
 
 var calc = new Calc();
 
-console.log( calc.add(5, 5), calc.sub(15, 5) );
+calc.set(3, 2);
+
+console.log( calc.add(), calc.sub() );
